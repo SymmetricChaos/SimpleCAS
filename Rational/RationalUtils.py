45bf524d9187782f26cda94debcf63dafcc3f0da
SymmetricChaos <ajfraebel@gmail.com>
2019-09-22 11:49:29 -0400

    Rational round

--- Rational/RationalUtils.py
+++ Rational/RationalUtils.py
@@ -76,11 +76,76 @@ def digits_to_frac(S):
         return Rational(int(S),10**D)
     else:
         raise Exception(f"Cannot coerce {S} to Rational")
+
+
+def cfrac_convergents(L):
+    """Rational convergents of a simple continued fraction."""
+    assert type(L) == list
+    for i in L:
+        assert type(i) == int
         
+    N = [1,L[0]]
+    D = [0,1]
+    con = 2
+    
+    yield Rational(N[-1],D[-1])
+    
+    while con < len(L)+1:
+        N.append( L[con-1] * N[con-1] + N[con-2] )
+        D.append( L[con-1] * D[con-1] + D[con-2] )
+
+        yield Rational(N[-1],D[-1])
         
+        con += 1
+
+
+def cfrac_to_frac(L):
+    """Convert a continued fraction to a Rational"""
+    assert type(L) == list, f"tpye {type(L)} is not a list"
+    for i in L:
+        assert type(i) == int
+    
+    N = [1,L[0]]
+    D = [0,1]
+    con = 2
+    
+    while con < len(L)+1:
+        N.append( L[con-1] * N[con-1] + N[con-2] )
+        D.append( L[con-1] * D[con-1] + D[con-2] )
+        con += 1
         
-        
-        
+    return Rational(N[-1],D[-1])
+
+
+def rational_round(Q,dlim):
+    """Best approximation of Q with denominator of d or less, by semi-convergents"""
+    assert type(Q) == Rational
+    assert type(dlim) == int
+    if dlim < 1:
+        raise ZeroDivisionError
+
+    # https://shreevatsa.wordpress.com/2011/01/10/not-all-best-rational-approximations-are-the-convergents-of-the-continued-fraction/
+    a = Q.cfrac()
+
+    semi = [0]
+    prev = Rational(0)
+    for pos,val in enumerate(a):
+        oldsemi = semi
+        semi = a[:pos]+[(val-1)//2+1]
+        if cfrac_to_frac(semi) - Q <= cfrac_to_frac(oldsemi) - Q:
+            semi[pos] += 1
+        while semi[pos] <= val:
+            if cfrac_to_frac(semi).d > dlim:
+                semi[pos] -= 1
+                return prev
+            prev = cfrac_to_frac(semi)
+            semi[pos] += 1
+    return Q
+
+
+
+
+
 if __name__ == '__main__':
     a = Rational(13,6)
     b = Rational(3,4)
@@ -89,6 +154,9 @@ if __name__ == '__main__':
     print(f"gcd({a},{b}) = {G}")
     print(f"lcm({a},{b}) = {L}")
     
-    d = "123.14"
+    d = "3.14159"
     r = digits_to_frac(d)
     print(f"{d} = {r} = {r.n/r.d}")
+    
+
+    print(rational_round(r,100))
