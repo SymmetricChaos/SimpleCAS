5501070a7e975ba720e4dea49a6b668ab4e15e2f
SymmetricChaos <ajfraebel@gmail.com>
2019-09-27 18:54:54 -0400

    string method

--- Roots/RootType.py
+++ Roots/RootType.py
@@ -1,20 +1,27 @@
 # Correct handling and evaluation of rational roots
 
-from Rational import Rational, str_to_frac, digits_to_frac
+from Rational import coerce_to_rational
 
 class Root:
     
     def __init__(self,n,contents):
-        if type(n) == Rational:
-            pass
-        elif type(n) == int:
-            n = Rational(n)
-        elif type(n) == str:
-            try:
-                n = str_to_frac(n)
-            except:
-                n = digits_to_frac(n)
-        else:
-            raise Exception(f"Could not coerce {n} to Rational")
+        self.n = coerce_to_rational(n).inv()
+        self.contents = contents
+    
+#    # Need to have this interact with various types for content
+#    def simplify()
+    
+    
+    # TODO: handle various inputs in a pretty way
+    def __str__(self):
+        p = f"{self.n}" if self.n.d == 1 else f"({self.n})"
+        return f"({self.contents})^{p}"
+    
 
-        self.n = n
+#    def __mul__(self,other):
+        
+
+
+if __name__ == '__main__':
+    r = Root("1/2",8)
+    print(r)
