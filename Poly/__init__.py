636c661ea4db3c896d3e0842887da65e7bc20dd3
SymmetricChaos <ajfraebel@gmail.com>
2019-09-28 07:25:26 -0400

    fix import

--- Poly/__init__.py
+++ Poly/__init__.py
@@ -1,4 +1,3 @@
 from Poly.QPoly import QPoly
 from Poly.QPolyUtils import poly_factor, rational_roots, lagrange_interpolation
-from Poly.ZPoly import ZPoly
-__all__=["QPoly", "ZPoly", "poly_factor", "rational_roots", "lagrange_interpolation"]
+__all__=["QPoly", "poly_factor", "rational_roots", "lagrange_interpolation"]
