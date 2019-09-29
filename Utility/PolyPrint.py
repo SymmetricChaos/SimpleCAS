7b14df76a8a345ca878f6a2a8991213e37560648
SymmetricChaos <ajfraebel@gmail.com>
2019-09-29 08:39:30 -0400

    Move poly prints

---
+++ Utility/PolyPrint.py
@@ -0,0 +1,182 @@
+def poly_print(poly):
+    """Show the polynomial in descending form with commandline formatting"""
+
+        
+    # Get the degree of the polynomial in case it is in non-normal form
+    d = poly.degree()
+    
+    if d == -1:
+        return f"0"
+
+    out = ""
+    
+    # Step through the ascending list of coefficients backward
+    # We do this because polynomials are usually written in descending order
+    for pwr in range(d,-1,-1):
+        
+        # Skip the zero coefficients entirely
+        if poly[pwr] == 0:
+            continue
+        
+        coe = poly[pwr]
+        val = abs(coe)
+        sgn = "-" if coe//val == -1 else "+"
+        
+        # Handle sign for leading term
+        if pwr == d and sgn == "+":
+            sgn = ""
+            
+                
+        # When the coefficient is 1 or -1 don't print it unless it is the
+        # coefficient for x^0
+        if val == 1 and pwr != 0:
+
+            if pwr == d:
+                if sgn == "+":
+                    sgn = ""
+                
+                # Handle powers of 1 or 0 that appear as the first term
+                if pwr == 1:
+                    s = f"{sgn}x"
+                else:
+                    s = f" {sgn}x^{pwr}"
+
+                            
+            
+            # If the power is 1 just show x rather than x^1
+            elif pwr == 1:
+                s = f" {sgn} x"
+            
+            # If the power is 0 only show the sign and value
+            elif pwr == 0:
+                s = f" {sgn} x"
+            
+            # Otherwise show everything
+            else:
+                s = f" {sgn} x^{pwr}"
+            out += s
+    
+        else:
+    
+            # If it is the first term include the sign of the coefficient
+            if pwr == d:
+                if sgn == "+":
+                    sgn = ""
+                
+                # Handle powers of 1 or 0 that appear as the first term
+                if pwr == 1:
+                    s = f"{sgn}{val}x"
+                elif pwr == 0:
+                    s = f"{sgn}{val}"
+                else:
+                    s = f" {sgn}{val}x^{pwr}"
+
+                            
+            
+            # If the power is 1 just show x rather than x^1
+            elif pwr == 1:
+                s = f" {sgn} {val}x"
+            
+            # If the power is 0 only show the sign and value
+            elif pwr == 0:
+                s = f" {sgn} {val}"
+            
+            # Otherwise show everything
+            else:
+                s = f" {sgn} {val}x^{pwr}"
+
+            out += s
+    
+    return out
+
+
+
+def poly_print_pretty(poly):
+    """Show the polynomial in descending form with LaTeX formatting"""
+
+        
+    # Get the degree of the polynomial in case it is in non-normal form
+    d = poly.degree()
+    
+    if d == -1:
+        return f"0"
+
+    out = ""
+    
+    # Step through the ascending list of coefficients backward
+    # We do this because polynomials are usually written in descending order
+    for pwr in range(d,-1,-1):
+        
+        
+        # Skip the zero coefficients entirely
+        if poly[pwr] == 0:
+            continue
+        
+        
+        coe = poly[pwr]
+        val = abs(coe)
+        sgn = u"\u2212" if coe//val == -1 else "+" #U+2212 is the large minus
+        
+        
+        # Handle sign for leading term
+        if pwr == d and sgn == "+":
+            sgn = ""
+            
+        
+        # Handle integer coefficients
+        if val.d == 1:
+            
+            # Handle the special case of coefficient 1 or -1
+            if val.n == 1:
+                
+                # Special case if term is x or -x
+                if pwr == 1:
+                    s = f" {sgn} $x$"
+                # Special case is term is 1 or -1
+                elif pwr == 0:
+                    s = f" {sgn} 1"
+                # General case
+                else:
+                    s = f" {sgn} $x^{{{pwr}}}$"
+
+            # General case
+            else:
+                if pwr == 1:
+                    s = f" {sgn} {val}$x$"
+                elif pwr == 0:
+                    s = f" {sgn} {val}"
+                else:
+                    s = f" {sgn} {val}$x^{{{pwr}}}$"
+
+        # Handle non-integer coefficients
+        else:
+            
+            # Handle special case of numerator 1
+            if val.n == 1:
+                
+                # Special case of term x/d
+                if pwr == 1:
+                    s = f" {sgn} $\dfrac{{x}}{{{val.d}}}$"
+                # Special case of term n/d
+                elif pwr == 0:
+                    s = f" {sgn} $\dfrac{{{val.n}}}{{{val.d}}}$"
+                # General case
+                else:
+                    s = f" {sgn} $\dfrac{{x^{{{pwr}}}}}{{{val.d}}}$"
+                
+            # General case
+            else:
+                if pwr == 1:
+                    s = f" {sgn} $\dfrac{{{val.n}x}}{{{val.d}}}$"
+                elif pwr == 0:
+                    s = f" {sgn} $\dfrac{{{val.n}}}{{{val.d}}}$"
+                else:
+                    s = f" {sgn} $\dfrac{{{val.n}x^{{{pwr}}}}}{{{val.d}}}$"
+        
+        # Special case of leading coefficient
+        if pwr == d:
+            s = s.replace(" ","")
+            
+        out += s
+    
+    return out
--- Utility/__init__.py
+++ Utility/__init__.py
@@ -1,9 +1,13 @@
-from Utility.Utils import egcd, gcd, lcm, poly_add, poly_mult, poly_print, \
-                            inds_where, first_where, factorization, \
-                            poly_print_pretty
+from Utility.Utils import egcd, gcd, lcm, poly_add, poly_mult, \
+                            inds_where, first_where, factorization
+                            
 from Utility.Drawing import make_canvas, plot_points, scatter_points, show_plot
+
 from Utility.ListManip import lists_to_tuples, tuples_to_lists
-__all__=["egcd", "gcd", "lcm", "poly_add", "poly_mult", "poly_print",
+
+from Utility.PolyPrint import poly_print, poly_print_pretty
+
+__all__=["egcd", "gcd", "lcm", "poly_add", "poly_mult", 
          "inds_where", "first_where", "factorization", "make_canvas", 
          "plot_points", "scatter_points", "lists_to_tuples", "tuples_to_lists",
-         "poly_print_pretty", "show_plot"]
+         "poly_print_pretty", "poly_print", "show_plot"]
