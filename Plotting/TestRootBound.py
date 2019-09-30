9d435a0f43d9c1b43b6d2128f0a06ee1d7e5efad
SymmetricChaos <ajfraebel@gmail.com>
2019-09-30 08:14:53 -0400

    Visual Test for Root Bounding

---
+++ Plotting/TestRootBound.py
@@ -0,0 +1,16 @@
+from Rational import rational_seq
+from Utility import make_canvas, plot_points
+from Poly import QPoly, bound_of_roots
+from random import randint
+
+co = [randint(-9,9) for i in range(4)]
+P = QPoly( co )
+b = (bound_of_roots(P)+1)//1
+x = rational_seq(-b,b,.05)
+y = [float(i) for i in P.evaluate(x)]
+
+pts = [i for i in zip(x,y)]
+
+make_canvas([-b,b],show_axes=True,title=P.pretty_name())
+
+plot_points(pts)
--- Poly/__init__.py
+++ Poly/__init__.py
@@ -1,3 +1,5 @@
 from Poly.QPoly import QPoly
 from Poly.QPolyUtils import poly_factor, rational_roots, lagrange_interpolation
-__all__=["QPoly", "poly_factor", "rational_roots", "lagrange_interpolation"]
+from Poly.RootApproximation import bound_of_roots
+__all__=["QPoly", "poly_factor", "rational_roots", "lagrange_interpolation",
+         "bound_of_roots"]
