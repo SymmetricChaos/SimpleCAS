ee92d31addf8970975a24b8758963df67cd0277f
SymmetricChaos <ajfraebel@gmail.com>
2019-09-29 20:35:26 -0400

    Sturm Chain

--- Poly/RootApproximation.py
+++ Poly/RootApproximation.py
@@ -17,7 +17,9 @@ def newtons_method(poly,start,den_lim=100,iter_lim=100):
     
     return r
 
-
+## TODO: make sure this actually makes sense
+## TODO: denominator limit
+## TODO: test with higher order polynomials
 def bisection_method(poly,lo,hi,iter_lim=10):
     assert type(poly) == QPoly
     
@@ -43,6 +45,25 @@ def bisection_method(poly,lo,hi,iter_lim=10):
     return out
 
 
+def sturm_chain(poly):
+    assert type(poly) == QPoly
+    
+    p0 = poly
+    p1 = poly.derivative()
+    
+    yield p0
+
+    while True:
+        p0, p1 = p1, -(p0 % p1)
+        yield p0
+        
+        if len(p0) == 1:
+            break
+        
+
+
+
+
 if __name__ == '__main__':
     R = QPoly( [-3,1,1] )
     approx_root = newtons_method(R,2)
@@ -51,7 +72,16 @@ if __name__ == '__main__':
     print(f"by Newton's method R has a root at approximately: {approx_root}\nwhich is {approx_root.digits(5)}")
     print(f"Approximation has an error of about {float(R(approx_root))}")
     
+    print("\n\n")
     
-    R = QPoly( [-3,1,1] )
-    approx_root = bisection_method(R,0,2)
-    print(float(approx_root[0]))
+    approx_root = bisection_method(R,0,2,10)[0]
+
+    print(f"R = {R}")
+    print(f"by the bisection method R has a root at approximately: {approx_root}\nwhich is {approx_root.digits(5)}")
+    print(f"Approximation has an error of about {float(R(approx_root))}")
+    
+    print("\n\n")
+    
+    print("Sturm Chain")
+    for i in sturm_chain( QPoly( [-1,-1,0,1,1] ) ):
+        print(i)
