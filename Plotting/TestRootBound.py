86cfe2484c8a84091f1b970d975b78ba5f82dcab
SymmetricChaos <ajfraebel@gmail.com>
2019-10-01 19:54:06 -0400

    Shows the "interesting big" of the polynomial

--- Plotting/TestRootBound.py
+++ Plotting/TestRootBound.py
@@ -1,18 +1,32 @@
 from Rational import rational_seq
-from Utility import make_canvas, plot_points
-from Poly import QPoly, all_roots
+from Utility import make_canvas, plot_points, scatter_points
+from Poly import QPoly, all_roots, critical_points
 from random import randint
 
-co = [randint(-9,9) for i in range(4)]
+co = [randint(-9,9) for i in range(5)]
 P = QPoly( co )
+print(f"P = {P}")
 r = all_roots(P)
-xwidth = [ float(r[0]-1), float(r[-1]+1) ]
-x = rational_seq(r[0]-1,r[-1]+1,.1)
+print(f"roots = {r}")
+c = critical_points(P)
+print(f"critical = {c}")
+
+interesting_points = r + c
+interesting_values = P.evaluate(interesting_points)
+
+
+
+
+xwidth = [ float(min(interesting_points)-1), float(max(interesting_points)+1) ]
+ywidth = [ float(min(interesting_values)-1), float(max(interesting_values)+1) ]
+
+x = rational_seq(xwidth[0],xwidth[1],.05)
 y = [float(i) for i in P.evaluate(x)]
 x = [float(i) for i in x]
-ywidth = (min(y),max(y))
+
 
 pts = [i for i in zip(x,y)]
 make_canvas(xwidth,ywidth,size=[5,5],show_axes=True,title=P.pretty_name())
 
 plot_points(pts)
+
