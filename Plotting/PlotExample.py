7dd31702a9ce0e558f3c3e987493fc016979540a
SymmetricChaos <ajfraebel@gmail.com>
2019-10-01 08:53:01 -0400

    import error

--- Plotting/PlotExample.py
+++ Plotting/PlotExample.py
@@ -7,19 +7,18 @@ from Poly import QPoly
 P = QPoly( [0,1,0,"-1/6",0,"1/120",0,"-1/5040"] )
 x = rational_seq(-7,7,.1)
 y = P.evaluate(x)
-
 pts = [i for i in zip(x,y)]
 
 make_canvas([-6,6],show_axes=True,title=P.pretty_name())
-
 plot_points(pts)
 
 
-R = RationalFunc(P,[-1,1])
-make_canvas([-6,6],show_axes=True,title=R.pretty_name())
 
+
+R = RationalFunc(P,[-1,1])
 x = rational_seq(-7,7,.1)
 y = R.evaluate(x)
 pts = [i for i in zip(x,y)]
 
+make_canvas([-6,6],show_axes=True,title=R.pretty_name())
 plot_points(pts)
--- Poly/__init__.py
+++ Poly/__init__.py
@@ -1,6 +1,6 @@
 from Poly.QPoly import QPoly
 from Poly.QPolyUtils import poly_factor, rational_roots, lagrange_interpolation
 from Poly.RootApproximation import bound_of_roots, newtons_method, \
-                                    sturm_root_isolation,
+                                    sturm_root_isolation
 __all__=["QPoly","poly_factor","rational_roots","lagrange_interpolation",
          "bound_of_roots","newtons_method","sturm_root_isolation"]
