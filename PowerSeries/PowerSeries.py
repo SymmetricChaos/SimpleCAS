d2738f440d9ef9506cb98c5fa81c5daef27bc12a
SymmetricChaos <ajfraebel@gmail.com>
2019-10-05 08:07:51 -0400

    Update PowerSeries.py

--- PowerSeries/PowerSeries.py
+++ PowerSeries/PowerSeries.py
@@ -1,5 +1,5 @@
 from Rational import coerce_to_rational
-
+from Poly import QPoly
 
 class PSeries:
     
@@ -11,7 +11,25 @@ class PSeries:
         self.a = a
         self.c = coerce_to_rational(c)
         
+        
+    def truncate(self,N):
+        """Truncates the power series"""
+        return PSeries(self.a[:N],self.c)
+    
+    def cast_to_poly(self,N):
+        """Truncates the power series and returns a polynomial"""
+        P = QPoly([0])
+        x = QPoly([0,1])
+        for pos,val in enumerate(self.a):
+            if pos > N:
+                break
+            P += val*(x-self.c)**pos
+        return P
+        
+        
 if __name__ == '__main__':
-    P = PSeries([5,6,5,5,6],4)
+    P = PSeries([1,1,1,1,1,1],4)
     print(P.a)
     print(P.c)
+    
+    print(P.cast_to_poly(3))
