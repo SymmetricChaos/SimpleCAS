213fd49007753220be73ede2f40b7ae3ccad5e37
SymmetricChaos <ajfraebel@gmail.com>
2019-10-05 11:47:43 -0400

    Evaluate method

--- PowerSeries/PowerSeries.py
+++ PowerSeries/PowerSeries.py
@@ -15,7 +15,8 @@ class PSeries:
     def truncate(self,N):
         """Truncates the power series"""
         return PSeries(self.a[:N],self.c)
-    
+
+
     def cast_to_poly(self,N):
         """Truncates the power series and returns a polynomial"""
         P = QPoly([0])
@@ -25,6 +26,27 @@ class PSeries:
                 break
             P += val*(x-self.c)**pos
         return P
+
+
+    def evaluate(self,x,N=None):
+        
+        if N == None:
+            try:
+                N = len(self.a)
+            except:
+                raise Exception("When a has no len() N must be supplied")
+        else:
+            N = N
+        
+        x = coerce_to_rational(x)
+        
+        out = 0
+        for pos,val in enumerate(self.a):
+            if pos > N:
+                break
+            out += val*(x-self.c)**pos
+            
+        return out
         
         
 if __name__ == '__main__':
@@ -33,3 +55,4 @@ if __name__ == '__main__':
     print(P.c)
     
     print(P.cast_to_poly(3))
+    print(P.evaluate(6))
