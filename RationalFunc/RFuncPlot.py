1dd7723fb59a488e78b6d496b2fb2035301ed82a
SymmetricChaos <ajfraebel@gmail.com>
2019-10-05 06:34:32 -0400

    Update RFuncPlot.py
    still bad

--- RationalFunc/RFuncPlot.py
+++ RationalFunc/RFuncPlot.py
@@ -1,40 +1,40 @@
 from Rational import rational_seq
-from Utility import make_canvas, plot_points, connect
+from Utility import make_canvas, plot_points, scatter_points, connect
 from RationalFunc import RFunc, rfunc_roots
 
 
 def rfunc_plot(rfunc):
     """Automatically make a plot that shows the rational function well"""
 
-    r = rfunc_roots(rfunc)
+    r = rfunc_roots(rfunc) + rfunc_roots(rfunc.derivative())
 
 
-#    # Pick the minimum possible axes for the graph
+    # Pick the minimum possible axes for the graph
     xlimit = [ min(r), max(r) ]
     ylimit = [ min(r), max(r) ]
     
-#    # Choose how much to pad
+    # Choose how much to pad
     xmargin = max(abs(xlimit[0]-xlimit[1])/10,1)
     ymargin = max(abs(ylimit[0]-ylimit[1])/10,1)
     
-#    # create the actual axes
+    # create the actual axes
     xwidth = [float(xlimit[0]-xmargin), float(xlimit[1]+xmargin)]
     ywidth = [float(ylimit[0]-ymargin), float(ylimit[1]+ymargin)]
     
     
-    x = rational_seq(xwidth[0],xwidth[1],.01)
+    x = rational_seq(xwidth[0],xwidth[1],.02)
     y = [float(i) for i in rfunc.evaluate(x)]
-    for pos,val in enumerate(y):
-        if val > ywidth[1]:
-            y[pos] = float('NaN')
-        if val < ywidth[0]:
-            y[pos] = float('NaN')
+#    for pos,val in enumerate(y):
+#        if val > ywidth[1]:
+#            y[pos] = float('NaN')
+#        if val < ywidth[0]:
+#            y[pos] = float('NaN')
     x = [float(i) for i in x]
     
     
     pts = [i for i in zip(x,y)]
     make_canvas(xwidth,ywidth,size=[5,5],show_axes=True,title=rfunc.pretty_name)
-    plot_points(pts,color="black")
+    scatter_points(pts,color="black")
     
     connect([xwidth[0],0],[xwidth[1],0],color="gray",zorder=-1)
     
