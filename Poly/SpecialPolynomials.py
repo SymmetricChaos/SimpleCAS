472e60f95f9b52e5eba977801163713ac7dbe22c
SymmetricChaos <ajfraebel@gmail.com>
2019-10-06 13:00:56 -0400

    Rising and Falling Factorials

--- Poly/SpecialPolynomials.py
+++ Poly/SpecialPolynomials.py
@@ -1,6 +1,7 @@
 from QPoly import QPoly
 
 def abel_poly(n,a):
+    """Abel polynomial"""
     if n == 0:
         return QPoly([1])
         
@@ -9,21 +10,81 @@ def abel_poly(n,a):
     return x*(x-a*n)**(n-1)
 
 
-def chebyshev_poly(n):
-    if n == 0:
-        return QPoly([1])
-    elif n == 1:
-        return QPoly([0,1])
-    else:
-        return 2*QPoly([0,1])*chebyshev_poly(n-1) - chebyshev_poly(n-2)
+def chebyshev_poly(n,kind=1):
+    """Chebyshev polynomial"""
+    if kind == 1:
+        
+        if n == 0:
+            return QPoly([1])
+        elif n == 1:
+            return QPoly([0,1])
+        else:
+            return QPoly([0,2])*chebyshev_poly(n-1) - chebyshev_poly(n-2)
     
+    elif kind == 2:
+        
+        if n == 0:
+            return QPoly([1])
+        elif n == 1:
+            return QPoly([0,2])
+        else:
+            return QPoly([0,2])*chebyshev_poly(n-1) - chebyshev_poly(n-2)
+
+    else:
+        raise Exception(f"There are only Chebyshev polynomials of the 1st and 2nd kind")
+
+
+def falling_factorial(n):
+    """Polynomial representation of the falling factorial"""
+    x = QPoly([0,1])
+    out = 1
+    for k in range(n):
+        out *= (x-k)
+    return out
+
+
+def falling_factorial_gen(n):
+    """Generator polynomial representations of the falling factorial"""
+    x = QPoly([0,1])
+    out = 1
+    for k in range(n):
+        yield out
+        out *= (x-k)
+
+
+def rising_factorial(n):
+    """Polynomial representation of the rising factorial"""
+    x = QPoly([0,1])
+    out = 1
+    for k in range(n):
+        out *= (x+k)
+    return out
+
+
+def rising_factorial_gen(n):
+    """Generator polynomial representations of the rising factorial"""
+    x = QPoly([0,1])
+    out = 1
+    for k in range(n):
+        yield out
+        out *= (x+k)
+
 
 if __name__ == '__main__':
     
     print("Abel Polynomials for a = 1")
     for n in range(6):
-        print(f"p_{n} =",abel_poly(n,1))
+        print(abel_poly(n,1))
         
     print("\n\nChebyshev Polynomials")
-    for n in range(6):
-        print(f"T_{n} =",chebyshev_poly(n))
+    for i in range(6):
+        print(chebyshev_poly(i))
+        
+    print("\n\nFalling Factorial Polynomials")
+    for i in falling_factorial_gen(6):
+        print(i)
+        
+    print("\n\nRising Factorial Polynomials")
+    for i in rising_factorial_gen(6):
+        print(i)
+        
