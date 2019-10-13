5caea5121544580234bc1eae92ecac936f1affd5
SymmetricChaos <ajfraebel@gmail.com>
2019-10-13 10:46:45 -0400

    poly_plot

--- Poly/__init__.py
+++ Poly/__init__.py
@@ -5,6 +5,9 @@ from Poly.QPolyUtils import poly_factor, rational_roots, \
 from Poly.RootApproximation import bound_of_roots, newtons_method, \
                                     sturm_root_isolation, qpoly_roots, \
                                     stationary_points, inflection_points
+                                    
+from Poly.PolyPlot import poly_plot
 __all__=["QPoly","poly_factor","rational_roots","lagrange_interpolation",
          "bound_of_roots","newtons_method","sturm_root_isolation",
-         "qpoly_roots","stationary_points","inflection_points","poly_gcd"]
+         "qpoly_roots","stationary_points","inflection_points","poly_gcd",
+         "poly_plot"]
