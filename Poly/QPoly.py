51babb9e0b6232f8490135322ddb2ecfd4a55d08
SymmetricChaos <ajfraebel@gmail.com>
2019-10-19 11:49:08 -0400

    QPolyQuo takes all of RFunc

--- Poly/QPoly.py
+++ Poly/QPoly.py
@@ -513,7 +513,9 @@ class QPolyProd:
 
 class QPolyQuo:
     
-    def __init__(self,N,D):
+    
+    def __init__(self,N,D=QPoly([1])):
+        
         if type(N) == QPoly:
             pass
         elif type(N) == list:
@@ -533,17 +535,31 @@ class QPolyQuo:
                 D = QPoly( [D] )
             except:
                 raise Exception(f"Could not coerce {D} to QPoly")
-        
-        
-        self.N = N 
+            
+        self.N = N
         self.D = D
+
         self.simplify()
+
 
     def simplify(self):
         # Remove common factors
         G = poly_gcd(self.N,self.D)
         self.N //= G
         self.D //= G
+        
+    
+    # Not really a thing for Rational Functions but still useful
+    def _content(self):
+        return self.N.content * self.D.content
+
+
+    def _primitive_part(self):
+        return QPolyQuo( self.N // self.content, self.D // self.content )
+        
+
+    def copy(self):
+        return QPolyQuo(self.N.copy(),self.D.copy())
 
 
     def __str__(self):
@@ -568,6 +584,94 @@ class QPolyQuo:
         return f"{n} / {d}"
 
 
+    def __call__(self,val):
+        n = self.N(val)
+        d = self.D(val)
+        
+        if d == 0:
+            return float('NaN')
+        else:
+            return n/d
+
+
+    def evaluate(self,val):
+        assert type(val) == list
+        return [self(v) for v in val]
+    
+
+    def inv(self):
+        return QPolyQuo(self.D,self.N)
+
+
+    def __mul__(self,other):
+        if type(other) == QPolyQuo:
+            return QPolyQuo(self.N * other.N, self.D * other.D)
+        else:
+            return self * QPolyQuo(other)
+
+
+    def __rmul__(self,other):
+        return self*other
+    
+    
+    def __pow__(self,pwr):
+        if type(pwr) != int:
+            raise TypeError(f"pwr must be an integer not {type(pwr)}")
+
+        if pwr == 0:
+            return QPolyQuo([1],[1])
+        else:
+            out = self.copy()
+            for i in range(abs(pwr)-1):
+                out *= self
+        
+        if pwr < 0:
+            return out.inv()
+            
+        return out
+        
+    
+    def __truediv__(self,other):
+        if type(other) == QPolyQuo:
+            return self * other.inv()
+        else:
+            return self / QPolyQuo(other)
+
+
+    def __neg__(self):
+        return self*-1
+
+
+    def __add__(self,other):
+        if type(other) == QPolyQuo:
+            return QPolyQuo(self.N*other.D + other.N*self.D, self.D*other.D)
+        elif type(other) == QPoly:
+            return self + QPolyQuo(other)
+        else:
+            return self + QPolyQuo([other])
+        
+
+    def __radd__(self,other):
+        return self + other
+
+
+    def __sub__(self,other):
+        if type(other) == QPolyQuo:
+            return self + -other
+        else:
+            return self - QPolyQuo(other)
+
+
+    def degree(self):
+        return max(self.N.degree(),self.D.degree())
+
+
+    def derivative(self):
+        N = (self.D * self.N.derivative()) - (self.N * self.D.derivative())
+        D = self.D * self.D
+        return QPolyQuo(N,D)
+
+
     def _pretty_name(self):
         if str(self.N) == "0":
             return "0"
@@ -583,7 +687,10 @@ class QPolyQuo:
             return f"${sgn}\dfrac{{{n}}}{{{self.D}}}$"
 
 
+    # Things that are like attributes can be access as properties
     pretty_name = property(_pretty_name)
+    content = property(_content)
+    primitive_part = property(_primitive_part)
 
 
 
