6f1bfc2b5c96b79377b47d22a2710c53ae03bebe
SymmetricChaos <ajfraebel@gmail.com>
2019-10-19 19:50:48 -0400

    all tests in one file
     so I don't miss stuff

--- Tests/UnitTests.py
+++ Tests/UnitTests.py
@@ -78,9 +78,35 @@ rf_minitests = [ (R, "(x + 1) / (3x + 2)"),
 
 
 
+#######################
+## Integration Tests ##
+#######################
+
+r = Rational(32,7)
+P = QPoly(["3/2",0,1,"11.6"])
+Q = QPoly([2])
+R = QPolyQuo( [1,1], [2,3] )
+
+i_minitests = [ (P+r, "58/5x^3 + x^2 + 85/14" ),
+                (r+P, "58/5x^3 + x^2 + 85/14" ),
+                (P*r, "1856/35x^3 + 32/7x^2 + 48/7" ),
+                (r*P, "1856/35x^3 + 32/7x^2 + 48/7" ),
+                (P//r, "203/80x^3 + 7/32x^2 + 21/64" ),
+                (r//Q, "16/7"),
+                (R+P, "(174/5x^4 + 131/5x^3 + 2x^2 + 11/2x + 4) / (3x + 2)"),
+                (P+R, "(174/5x^4 + 131/5x^3 + 2x^2 + 11/2x + 4) / (3x + 2)"),
+              ]
+
+
+
+
+
+
 print("\nTest Rational")
 unit_test(r_minitests)
 print("\nTest QPoly")
 unit_test(p_minitests)
 print("\nTest QPolyQuo")
 unit_test(rf_minitests)
+print("\nIntegration Tests")
+unit_test(i_minitests)
