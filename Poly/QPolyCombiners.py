b3b7a700ad5222c6f1f4c62b087020ae33dbeb11
SymmetricChaos <ajfraebel@gmail.com>
2019-10-19 15:29:30 -0400

    Moved sum and prod to their own files

---
+++ Poly/QPolyCombiners.py
@@ -0,0 +1,241 @@
+from collections import Counter
+from QPoly import QPoly
+
+
+
+class QPolySum:
+    
+    def __init__(self,terms):
+        if type(terms) == list:
+            self.terms = Counter(terms)
+        elif type(terms) == QPoly:
+            self.terms = Counter([terms])
+        elif type(terms) == Counter:
+            self.terms = terms
+        else:
+            raise TypeError("Not a Counter or list")
+            
+        self.simplify_const()
+
+
+    def simplify_const(self):
+        """Deal with constant terms"""
+        const = 0
+        remv = []
+        for i in self.terms.items():
+            if len(i[0]) == 1:
+                const += i[0]*i[1]
+                remv.append(i[0])
+        
+        for t in remv:
+            del self.terms[t]
+            
+        self.terms[const] = 1
+
+
+    def cast_to_poly(self):
+        """Add everything together as a QPoly"""
+        out = QPoly([0])
+        for val,mul in self.terms.items():
+            out += val*mul
+        return out
+    
+    
+    def copy(self):
+        """Copy the sum"""
+        return QPolySum(self.terms.copy())
+    
+    
+    def __add__(self,other):
+        A = self.terms.copy()
+        A.update(other.terms)
+        return QPolySum(A)
+    
+    
+    def __mul__(self,other):
+        assert type(other) == int
+        
+        if other == 0:
+            return QPolySum([QPoly([0])])
+        
+        A = self.terms.copy()
+        B = A.copy()
+        for i in range(other-1):
+            A.update(B)
+        return QPolySum(A)
+    
+
+    def __str__(self):
+        out = []
+        
+        S = []
+        for i in self.terms.keys():
+            S.append(i)
+        S.sort(key=len,reverse=True)
+        
+        for i in S:
+            mul = self.terms[i]
+            
+            if mul == 1:
+                if len(i) == 1:
+                    out.append(str(i))
+                else:
+                    out.append(f"({i})")
+            else:
+                if len(i) == 1:
+                    out.append(f"{mul}*{i}")
+                else:
+                    out.append(f"{mul}({i})")
+        
+        return " + ".join(out)
+
+
+    def _pretty_name(self):
+        out = []
+        
+        S = []
+        for i in self.terms.keys():
+            S.append(i)
+        S.sort(key=len)
+        
+        for i in S:
+            pwr = self.terms[i]
+            
+            if pwr == 1:
+                if len(i) == 1:
+                    out.append(str(i))
+                else:
+                    out.append(f"({i.pretty_name})")
+            else:
+                if len(i) == 1:
+                    out.append(f"{pwr}*{i.pretty_name}")
+                else:
+                    out.append(f"{pwr}({i.pretty_name})")
+        
+        J = "\;+\;".join(out)
+        J = J.replace("$","")
+        return f"${J}$"
+
+
+    # Things that are like attributes can be access as properties
+    pretty_name = property(_pretty_name)
+
+
+
+
+
+class QPolyProd:
+    
+    def __init__(self,terms):
+        assert type(terms) == list
+        self.terms = Counter(terms)
+        self.simplify_const()
+
+
+    def simplify_const(self):
+        """Deal with constant terms"""
+        const = 1
+        remv = []
+        for i in self.terms.items():
+            if len(i[0]) == 1:
+                const *= i[0]**i[1]
+                remv.append(i[0])
+        
+        for t in remv:
+            del self.terms[t]
+            
+        self.terms[const] = 1
+
+
+    def cast_to_poly(self):
+        """Multiply everything together as a QPoly"""
+        out = QPoly([1])
+        for val,pwr in self.terms.items():
+            out *= val**pwr
+        return out
+
+
+    def __str__(self):
+        out = []
+        
+        S = []
+        for i in self.terms.keys():
+            S.append(i)
+        S.sort(key=len)
+        
+        for i in S:
+            pwr = self.terms[i]
+            
+            if pwr == 1:
+                if len(i) == 1:
+                    out.append(str(i))
+                else:
+                    out.append(f"({i})")
+            else:
+                if len(i) == 1:
+                    out.append(f"{i}^{pwr}")
+                else:
+                    out.append(f"({i})^{pwr}")
+        
+        return "".join(out)
+
+
+    def _pretty_name(self):
+        out = []
+        
+        S = []
+        for i in self.terms.keys():
+            S.append(i)
+        S.sort(key=len)
+        
+        for i in S:
+            pwr = self.terms[i]
+            
+            if pwr == 1:
+                if len(i) == 1:
+                    out.append(str(i))
+                else:
+                    out.append(f"({i.pretty_name})")
+            else:
+                if len(i) == 1:
+                    out.append(f"{i.pretty_name}^{{{pwr}}}")
+                else:
+                    out.append(f"({i.pretty_name})^{{{pwr}}}")
+        
+        J = "\;".join(out)
+        J = J.replace("$","")
+        return f"${J}$"
+
+
+    # Things that are like attributes can be access as properties
+    pretty_name = property(_pretty_name)
+
+
+
+
+
+if __name__ == '__main__':
+
+    P = QPoly([1,2,3])
+    Q = QPoly([6,-2])
+    R = QPoly([5])
+    S = QPoly([7])
+    
+    sum_of_polys = QPolySum([P,S,Q,P,R,R,R])
+    print(sum_of_polys)
+    print(sum_of_polys.cast_to_poly())
+    
+    print()
+    
+    prod_of_polys = QPolyProd([Q,S,Q,P,S])
+    print(prod_of_polys)
+    print(prod_of_polys.cast_to_poly())
+    
+    print()
+    
+    print((P)/(Q*P))
+    
+    print()
+    
+    print(sum_of_polys * 3)
+    print(sum_of_polys * 0)
--- Poly/__init__.py
+++ Poly/__init__.py
@@ -1,11 +1,14 @@
-from Poly.QPoly import QPoly, poly_gcd, QPolySum, QPolyProd, QPolyQuo
+from Poly.QPoly import QPoly, poly_gcd, QPolyQuo
+
+from Poly.QPolyCombiners import QPolySum, QPolyProd
+
 from Poly.QPolyUtils import poly_factor, rational_roots, \
                             lagrange_interpolation, poly_egcd
 
 from Poly.RootApproximation import bound_of_roots, newtons_method, \
                                     sturm_root_isolation, qpoly_roots, \
                                     stationary_points, inflection_points
-                                    
+
 from Poly.PolyPlot import poly_plot
 
 __all__=["QPoly","QPolySum","QPolyProd","QPolyQuo","poly_factor",
