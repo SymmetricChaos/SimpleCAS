56fbae92214ff80a05c3bdedc3e1a6acdff44e3c
SymmetricChaos <ajfraebel@gmail.com>
2019-10-20 13:55:34 -0400

    RFunc is now obsolete and part of QPoly

--- RationalFunc/__init__.py
+++
@@ -1,4 +0,0 @@
-from RationalFunc.RationalFunction import RFunc
-from RationalFunc.RationalFunctionUtils import rfunc_roots
-                                                           
-__all__ = ["RFunc","rfunc_roots"]
