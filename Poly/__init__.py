03b3f193faacd06ce6369edd87c325043607fe5c
SymmetricChaos <ajfraebel@gmail.com>
2019-10-25 08:55:34 -0400

    import

--- Poly/__init__.py
+++ Poly/__init__.py
@@ -1,7 +1,5 @@
 from Poly.QPoly import QPoly, poly_gcd, RFunc
 
-from Poly.ZPoly import ZPoly
-
 from Poly.QPolyCombiners import QPolySum, QPolyProd
 
 from Poly.QPolyUtils import poly_factor, rational_roots, \
@@ -15,9 +13,17 @@ from Poly.RootApproximation import bound_of_roots, newtons_method, \
 
 from Poly.PolyPlot import poly_plot
 
+from Poly.ZPoly import ZPoly
+
+from Poly.ZPolyPlot import zpoly_plot
+
+from Poly.ZPolyUtils import zpoly_lagrange_interpolation, make_shamir_secret, \
+                            get_shamir_secret
+
 __all__=["QPoly","QPolySum","QPolyProd","RFunc","poly_factor",
          "rational_roots","lagrange_interpolation",
          "bound_of_roots","newtons_method","sturm_root_isolation",
          "qpoly_roots","stationary_points","inflection_points","poly_gcd",
          "poly_egcd","rfunc_roots","rfunc_asymptotes","poly_plot",
-         "ZPoly"]
+         "ZPoly","zpoly_plot","zpoly_lagrange_interpolation",
+         "make_shamir_secret","get_shamir_secret"]
