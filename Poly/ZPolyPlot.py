044e33e779d89f52b742a015836363f6ef5ebfd1
SymmetricChaos <ajfraebel@gmail.com>
2019-10-26 09:28:28 -0400

    zpoly_points
    Getting all points for ZPoly might be useful so that is its own function now

--- Poly/ZPolyPlot.py
+++ Poly/ZPolyPlot.py
@@ -2,23 +2,25 @@ from Utility import make_canvas, scatter_points, show_plot
 from Poly import ZPoly
  
 
-def zpoly_plot(poly,size=[5,5],show_plot=True):
+def zpoly_points(poly):
+    x = [n for n in range(F)]
+    y = poly.evaluate(x)
+    
+    return [i for i in zip(x,y)]
+
+
+def zpoly_plot(poly,size=[5,5]):
     """Automatically make a plot that shows the roots and critical points of the polynomial"""
     
     F = poly.F
     xwidth = [-1,F+1]
     ywidth = [-1,F+1]
 
-    x = [n for n in range(F)]
-    y = [i for i in poly.evaluate(x)]
+    pts = zpoly_points(poly)
     
-    pts = [i for i in zip(x,y)]
-    
-    if show_plot:
-        make_canvas(xwidth,ywidth,size=size,show_axes=True,title=f"{poly.pretty_name}   (mod {F})")
-        scatter_points(pts)
+    make_canvas(xwidth,ywidth,size=size,show_axes=True,title=f"{poly.pretty_name}   (mod {F})")
+    scatter_points(pts)
         
-
     return pts
 
 
@@ -32,9 +34,5 @@ if __name__ == '__main__':
     P = ZPoly( coefs, F )
     print(f"P = {P} (mod {P.F})")
     
-    pts = zpoly_plot(P)
+    zpoly_plot(P)
     show_plot()
-#    print()
-#    for i in pts:
-#        print(i)
-    
