ce6d407b71fa000e26fa8d4d8898face5c6ee93b
SymmetricChaos <ajfraebel@gmail.com>
2019-11-16 15:48:33 -0500

    Rational Points

--- Geometry/PythagoreanTriples.py
+++ Geometry/PythagoreanTriples.py
@@ -1,7 +1,7 @@
 from Utility import gcd
 
 def prim_pythag_triples(N):
-
+    """N is the limit of m and n in Euclid's formula"""
     
     for m in range(1,N):
         for n in range(m+1,N):
@@ -12,12 +12,24 @@ def prim_pythag_triples(N):
                     c = m*m+n*n
                     
                     yield sorted([a,b,c])
-            
 
+            
 
 
             
 if __name__ == '__main__':
-    
-    for i in prim_pythag_triples(7):
+    from Utility import make_canvas, scatter_points
+        
+    for i in prim_pythag_triples(10):
         print(i)
+        
+    make_canvas([-1.2,1.2],size=6,title="Rational Points on a Circle")
+    pts = []
+    for i in prim_pythag_triples(10):
+        pts += [[i[0]/i[2],i[1]/i[2]]]
+        pts += [[-i[0]/i[2],i[1]/i[2]]]
+        pts += [[i[0]/i[2],-i[1]/i[2]]]
+        pts += [[-i[0]/i[2],-i[1]/i[2]]]
+
+    scatter_points(pts,s=2)
+    
