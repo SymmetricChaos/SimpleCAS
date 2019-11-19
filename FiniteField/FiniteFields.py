0aa5f900ccc6c33529e3c8cfba06e20d6387dc2b
SymmetricChaos <ajfraebel@gmail.com>
2019-11-19 07:38:18 -0500

    more flexible finite fields

--- FiniteField/FiniteFields.py
+++ FiniteField/FiniteFields.py
@@ -5,22 +5,26 @@ import pprint
 
 # work in division, gcd for polynomials
 
-def finite_field_example():
-    print("GF(16)")
+def finite_field_example(P):
 
-    
-    P = ZPoly( [1,1,0,0,1], 2 )
-    S = ZPoly( [0,1], 2 )
-    out = ZPoly( [1], 2 )
+
+    S = ZPoly( [0,1], P.M )
+    out = ZPoly( [1], P.M )
     
     Fnum = {0:P}
     Fpol = {P:0, ZPoly([0],2):0}
     
-    for i in range(1,16):
+    elems = (P.M)**(len(P)-1)
+    
+    for i in range(1,elems):
+        if out in Fpol:
+            raise Exception("Not a finite field")
         Fnum[i] = out
         Fpol[out] = i
         out = (out * S) % P
-        
+
+    
+    print(f"GF({elems})")    
     pprint.pprint(Fnum)
     
     return Fnum,Fpol
@@ -30,10 +34,22 @@ def finite_field_example():
     
 if __name__ == '__main__':
     import random
-    Fnum,Fpol = finite_field_example()
     
-    print()
-    for i in range(5):
+    P = ZPoly( [1,1,0,0,1], 2 )
+    Fnum,Fpol = finite_field_example(P)
+    
+    print("\n\n")
+    for i in range(3):
+        x,y = random.sample(list(Fnum),2)
+        z = Fnum[0]
+        a = Fnum[x]
+        b = Fnum[y]
+        print(f"({a}) + ({b}) = {(a+b)%z}")
+        print(f"{Fpol[a]} + {Fpol[b]} = {Fpol[(a+b)%z]}")
+        print()
+    
+    print("\n\n")
+    for i in range(3):
         x,y = random.sample(list(Fnum),2)
         z = Fnum[0]
         a = Fnum[x]
@@ -41,9 +57,9 @@ if __name__ == '__main__':
         print(f"({a}) * ({b}) = {(a*b)%z}")
         print(f"{Fpol[a]} * {Fpol[b]} = {Fpol[(a*b)%z]}")
         print()
-        print()
-
-    for i in range(5):
+    
+    print("\n\n")
+    for i in range(3):
         x,y = random.sample(list(Fnum),2)
         z = Fnum[0]
         a = Fnum[x]
