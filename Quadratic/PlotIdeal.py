27b2665b5749bc864dd9b0d2840af0bccd7eb07a
SymmetricChaos <ajfraebel@gmail.com>
2019-12-29 08:16:28 -0500

    weird error with make_canvas

--- Quadratic/PlotIdeal.py
+++ Quadratic/PlotIdeal.py
@@ -2,10 +2,10 @@ from Quadratic.GaussianIntegers import GaussInt, ideal
 from Utils.Drawing import make_canvas, scatter_points, connect
 
 
-
-make_canvas([-6,6],[-6,6],show_axes = True)
-
 G = GaussInt(1,2)
+
+make_canvas([-6,6],[-6,6],size=8,show_axes=True,title="Ideal")
+
 
 pts = []
 
@@ -22,3 +22,4 @@ connect([0,-10],[0,10],color="gray",zorder=0)
 for i in range(-10,10):
     connect([-10,i],[10,i],color="lightgray",zorder=-1)
     connect([i,-10],[i,10],color="lightgray",zorder=-1)
+    
