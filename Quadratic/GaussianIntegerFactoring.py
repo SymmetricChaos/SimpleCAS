f5bbbdd0c8f3aeb76156768c09881e33a0faa158
SymmetricChaos <ajfraebel@gmail.com>
2019-12-31 18:37:15 -0500

    Update GaussianIntegerFactoring.py

--- Quadratic/GaussianIntegerFactoring.py
+++ Quadratic/GaussianIntegerFactoring.py
@@ -1,7 +1,7 @@
 from Utility.Math import prime_factorization
 from Quadratic.GaussianIntegers import GaussInt, str_to_gauss
 
-def is_guass_prime(G):
+def is_gauss_prime(G):
     assert type(G) == GaussInt
     
     if G.re == 0:
@@ -18,13 +18,18 @@ def is_guass_prime(G):
     
     return False
 
-
-#def gauss_factorization(G):
-    
+# The norm is a multiplcative function
+# Thus if we can factor the norm we known the norms of the factors
+def gauss_factorization(G):
+    F = prime_factorization(G.norm)
+    print(F)
 
 
 if __name__ == '__main__':
     L = ["5","2+5i","-2-5i","-11+7i","88-23i","15+22i"]
     for i in L:
         g = str_to_gauss(i)
-        print(g,is_guass_prime(g))
+        print(g,is_gauss_prime(g))
+    
+    g = GaussInt(-11,7)
+    print(gauss_factorization(g))
