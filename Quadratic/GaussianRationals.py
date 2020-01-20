5ea984a6a5a7cbe7613901b09eb032e5fb0953ee
SymmetricChaos <ajfraebel@gmail.com>
2020-01-20 09:35:52 -0500

    Refactor Gaussian Rationals

--- Quadratic/GaussianRationals.py
+++ Quadratic/GaussianRationals.py
@@ -1,53 +1,63 @@
 from Quadratic.GaussianIntegers import GaussInt, gauss_gcd, str_to_gauss
-
+from Rational import Rational, rational_gcd, cast_to_rational
 
 class GaussRational:
     
-    def __init__(self,n=0,d=1):
+    def __init__(self,re,im):
         
-        if type(n) == str:
-            n = str_to_gauss(n)
-        if type(d) == str:
-            d = str_to_gauss(d)
-        
-        if type(n) not in [GaussInt,int]:
-            raise TypeError("Numerator must be GaussInt.")
-        if type(d) not in [GaussInt,int]:
-            raise TypeError("Denominator must be GaussInt.")
-        if d == 0:
-            raise ZeroDivisionError
+        self.re = cast_to_rational(re)
+        self.im = cast_to_rational(im)
 
-        if type(n) == int:
-            n = GaussInt(n,0)
-        if type(d) == int:
-            d = GaussInt(d,0)
-
-        self.n = n
-        self.d = d
-        self.simplify()
+#        self.simplify()
 
 
-    def simplify(self):
-        """Convert fraction to one of its reduced forms"""
-        g = gauss_gcd(self.n,self.d)
-        
-        # Divide out GCD
-        if g != 1 and g != -1:
-            self.n = self.n//g
-            self.d = self.d//g
-
-        # Force lower coefficient to have positive real part
-        if self.d.re < 0:
-            self.n = -self.n
-            self.d = -self.d
+#    def simplify(self):
+#        """Convert fraction to one of its reduced forms"""
+#        g = gauss_gcd(self.n,self.d)
+#        
+#        # Divide out GCD
+#        if g != 1 and g != -1:
+#            self.n = self.n//g
+#            self.d = self.d//g
+#
+#        # Force lower coefficient to have positive real part
+#        if self.d.re < 0:
+#            self.n = -self.n
+#            self.d = -self.d
         
 
     def _pretty_name(self):
         """Format for LaTeX"""
-        if self.d == 1:
-            return f"${self.n}$"
+        # If the imaginary part is zero return just the whole part
+        if self.im == Rational(0):
+            return f"${self.re}$"
+        
+        # If the real part is zero just return the imaginary part
+        if self.re == Rational(0):
+            if self.im == Rational(1):
+                return "$i$"
+            elif self.im == Rational(-1):
+                return "$-i$"
+            else:
+                return f"${self.im}i$"
+        
+        # If the imaginary part is negative
+        elif self.im < Rational(0):
+            # Unit case
+            if self.im == Rational(-1):
+                return f"${self.re} - i$"
+            # General case
+            else:
+                return f"${self.re} - {abs(self.im)}i$"
+        
+        # If the imaginary part is positive
         else:
-            return f"$\dfrac{{{self.n}}}{{{self.d}}}$"
+            # Unit case
+            if self.im == Rational(1):
+                return f"${self.re} + i$"
+            # General case
+            else:
+                return f"${self.re} + {self.im}i$"
 
 
     def copy(self):
@@ -55,20 +65,37 @@ class GaussRational:
 
 
     def __str__(self):
-        if self.d == 1:
-            return str(self.n)
         
-        if self.n.re == 0 or self.n.im == 0:
-            num = f"{self.n}"
+        # If the imaginary part is zero return just the whole part
+        if self.im == Rational(0):
+            return f"{self.re}"
+        
+        # If the real part is zero just return the imaginary part
+        if self.re == Rational(0):
+            if self.im == Rational(1):
+                return "i"
+            elif self.im == Rational(-1):
+                return "-i"
+            else:
+                return f"{self.im}i"
+        
+        # If the imaginary part is negative
+        elif self.im < Rational(0):
+            # Unit case
+            if self.im == Rational(-1):
+                return f"{self.re} - i"
+            # General case
+            else:
+                return f"{self.re} - {abs(self.im)}i"
+        
+        # If the imaginary part is positive
         else:
-            num = f"({self.n})"
-        
-        if self.d.re == 0 or self.d.im == 0:
-            den = f"{self.d}"
-        else:
-            den = f"({self.d})"
-        
-        return f"{num}/{den}"
+            # Unit case
+            if self.im == Rational(1):
+                return f"{self.re} + i"
+            # General case
+            else:
+                return f"{self.re} + {self.im}i"
 
 
     def __repr__(self):
@@ -76,24 +103,37 @@ class GaussRational:
 
 
     def inv(self):
-        return GaussRational(self.d,self.n)
+        a = self.re.n
+        b = self.re.d
+        c = self.im.n
+        d = self.im.d
+        
+        e = self.re**2 + self.im**2
+
+
+        A = a/(b*e)
+        B = -c/(d*e)
+
+        return GaussRational(A,B)
 
 
     def __neg__(self):
-        return GaussRational(-self.n,self.d)
+        return self*-1
 
 
-    def __add__(self,addend):
-        if type(addend) in [GaussInt,int]:
-            addend = GaussRational(addend)
-        
-        if type(addend) != GaussRational:
+    def __add__(self,other):
+        if type(other) == GaussRational:
+            return GaussRational(self.re+other.re,
+                                 self.im+other.im)
+        elif type(other) == int:
+            return GaussRational(self.re+other,self.im)
+        elif type(other) == Rational:
+            return GaussRational(self.re+other,self.im)
+        elif type(other) == GaussInt:
+            return GaussRational(self.re+other.re,
+                                 self.im+other.im)
+        else:
             return NotImplemented
-                
-        n = self.n*addend.d + addend.n*self.d
-        d = self.d*addend.d
-
-        return GaussRational(n,d)
 
 
     def __radd__(self,addend):
@@ -108,17 +148,21 @@ class GaussRational:
         return addend + -self
 
 
-    def __mul__(self,multiplier):
-        if type(multiplier) in [GaussInt,int]:
-            multiplier = GaussRational(multiplier)
-            
-        if type(multiplier) != GaussRational:
+    def __mul__(self,other):
+        if type(other) == GaussRational:
+            return GaussRational(self.re*other.re - self.im*other.im,
+                                 self.re*other.im + self.im*other.re)
+        elif type(other) == GaussInt:
+            return GaussRational(self.re*other.re - self.im*other.im,
+                                 self.re*other.im + self.im*other.re)
+        elif type(other) == int:
+            return GaussRational(other*self.re,
+                                 other*self.im)
+        elif type(other) == Rational:
+            return GaussRational(other*self.re,
+                                 other*self.im)
+        else:
             return NotImplemented
-            
-        n = self.n * multiplier.n
-        d = self.d * multiplier.d
-        
-        return GaussRational(n,d)
 
 
     def __rmul__(self,multiplier):
@@ -144,30 +188,30 @@ class GaussRational:
 
     # Because GCD is not unique it is possible for apparently different numbers
     # to be equal
-    def __eq__(self,other):
-        if type(other) == int:
-            other = GaussRational(other)
-        if type(other) == GaussRational:
-            return self.n*other.d == self.d*other.n
-        else:
-            return False
+#    def __eq__(self,other):
+#        if type(other) == int:
+#            other = GaussRational(other)
+#        if type(other) == GaussRational:
+#            return self.n*other.d == self.d*other.n
+#        else:
+#            return False
 
 
-    def __pow__(self,pwr):
-        if type(pwr) != int:
-            raise TypeError(f"pwr must be an integer not {type(pwr)}")
-        
-        # For negative powers invert then use recursion
-        if pwr < 0:
-            return self.inv()**abs(pwr)
-        elif pwr == 0:
-            return GaussRational(1)
-        elif pwr == 1:
-            return self
-        else:
-            n = self.n**pwr
-            d = self.d**pwr
-            return GaussRational(n,d)
+#    def __pow__(self,pwr):
+#        if type(pwr) != int:
+#            raise TypeError(f"pwr must be an integer not {type(pwr)}")
+#        
+#        # For negative powers invert then use recursion
+#        if pwr < 0:
+#            return self.inv()**abs(pwr)
+#        elif pwr == 0:
+#            return GaussRational(1)
+#        elif pwr == 1:
+#            return self
+#        else:
+#            n = self.n**pwr
+#            d = self.d**pwr
+#            return GaussRational(n,d)
 
 
     def __hash__(self):
@@ -182,14 +226,14 @@ class GaussRational:
 
 if __name__ == '__main__':
 
-    R = GaussRational("-3-3i","2+4i")
-    S = GaussRational("7-3i","-1+5i")
+    R = GaussRational("1/2","7/5")
+    S = GaussRational("4","9/7")
     print(f"R = {R}")
     print(f"-R = {-R}")
     print(f"1/R = {1/R}")
     print(f"R * 1/R = {R*1/R}")
     print(f"R*i = {R*GaussInt(0,1)}")
-    print(f"R**2 = {R**2}")
+#    print(f"R**2 = {R**2}")
     print(f"S = {S}")
     print(f"R*S = {R*S}")
     print(f"R/S = {R/S}")
